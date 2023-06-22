a9aba1a3882de20bb3f5275ae07cbfc9a27720b1
LNMIG <migliorenicolas@yahoo.com.ar>
2023-06-22 16:27:09 -0300

    Posts files updated. Models added many-to-many field

--- blogapp/posts/models.py
+++ blogapp/posts/models.py
@@ -34,28 +34,6 @@ from users.models import User
 #       return self.rol
 
 
-class Article(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=100, null=False, blank=False)
-    resume = models.CharField(max_length=200, null=True, blank=True)
-    content = models.TextField(null=False, blank=False)
-    image = models.CharField(max_length=500, null=True, blank=True)
-    is_active = models.BooleanField(default=True)
-    creation_date = models.DateTimeField('date created')
-
-    
-    def __str__(self):
-        return self.title
-
-
-class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    content = models.TextField(null=False, blank=False)
-    is_active = models.BooleanField(default=True)
-    creation_date = models.DateTimeField('date created')
-
-
 class Category(models.Model):
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=50, null=False, blank=False)
@@ -68,10 +46,32 @@ class Category(models.Model):
         return self.name
 
 
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100, null=False, blank=False)
+    resume = models.CharField(max_length=200, null=True, blank=True)
+    content = models.TextField(null=False, blank=False)
+    image = models.CharField(max_length=500, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    creation_date = models.DateTimeField('date created', auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='Category_Article', through_fields=('article', 'category'))
+
+    
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    content = models.TextField(null=False, blank=False)
+    is_active = models.BooleanField(default=True)
+    creation_date = models.DateTimeField('date created', auto_now_add=True)
+
+
 class Category_Article(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-
 
     # question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # choice_text = models.CharField(max_length=200)
