1c96ebaad93f0409ae3be2fb52ab4c6d210de984
LNMIG <migliorenicolas@yahoo.com.ar>
2023-06-22 16:33:44 -0300

    Blog app's urls-settings updated

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'blogapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,3 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/users/userindex'
